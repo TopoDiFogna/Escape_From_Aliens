ded2834e892a91f5c28daabd69b846ab12437834
Paolo Paterna <paolo.paterna@gmail.com>
2015-04-21 18:38:37 +0200

    fast forward

--- src/main/java/it/polimi/ingsw/cg_23/App.java
+++ src/main/java/it/polimi/ingsw/cg_23/App.java
@@ -8,10 +8,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        //System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
         //System.out.println( "Yay!" );
         System.out.println( "Test" );
         System.out.println( "Test" );
+        //fast forward
         
     }
 }
