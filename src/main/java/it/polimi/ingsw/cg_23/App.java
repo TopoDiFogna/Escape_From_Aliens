78b232c126c243653e76a8cfd3f223b35875e604
Arianna Zappa <zappa.arianna@gmail.com>
2015-04-24 10:08:46 +0200

    Test commit 3

--- src/main/java/it/polimi/ingsw/cg_23/App.java
+++ src/main/java/it/polimi/ingsw/cg_23/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        //yay
+        //Altro test
+        //altra riga di commento
     }
 }
