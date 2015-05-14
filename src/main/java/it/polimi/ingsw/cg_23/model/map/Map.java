1f9e0c2aa970628c9eba52f56d3493fb409362a8
Paolo Paterna <paolo.paterna@gmail.com>
2015-05-14 19:59:30 +0200

    Removed useless attributes and methods in map class Defined the map

--- src/main/java/it/polimi/ingsw/cg_23/model/map/Map.java
+++ src/main/java/it/polimi/ingsw/cg_23/model/map/Map.java
@@ -1,15 +1,8 @@
 package it.polimi.ingsw.cg_23.model.map;
 
-import it.polimi.ingsw.cg_23.model.map.SectorAlien;
-import it.polimi.ingsw.cg_23.model.map.SectorDangerous;
-import it.polimi.ingsw.cg_23.model.map.SectorEscapeHatch;
-import it.polimi.ingsw.cg_23.model.map.SectorHuman;
-import it.polimi.ingsw.cg_23.model.map.SectorSecure;
-import it.polimi.ingsw.cg_23.model.map.SectorVoid;
-
 /**
- * Description of Map. The map is where the game is played.
- * It's divided in sectors and has a maximum fixed size.
+ * The map is where the game is played.
+ * It's divided in sectors and has a maximum fixed size defined by SIZENUMBER and SIZELETTER.
  *  
  * @author Paolo
  */
@@ -23,36 +16,6 @@ public class Map {
      * Maximum rows in the map.
      */
     private final static int SIZELETTER = 14;
-    
-    /**
-     * Description of the property sectorDangerous.
-     */
-    private final SectorDangerous sectorDangerous = null;
-
-    /**
-     * Description of the property sectorSecure.
-     */
-    private final SectorSecure sectorSecure = null;
-
-    /**
-     * Description of the property sectorAlien.
-     */
-    private final SectorAlien sectorAlien = null;
-
-    /**
-     * Description of the property sectorVoid.
-     */
-    private SectorVoid sectorVoid = null;
-
-    /**
-     * Description of the property sectorHuman.
-     */
-    private final SectorHuman sectorHuman = null;
-
-    /**
-     * Description of the property sectorEscapeHatch.
-     */
-    private final SectorEscapeHatch sectorEscapeHatch = null;
 
     /**
      * Sectors of the map: rows x columns
@@ -61,11 +24,14 @@ public class Map {
     private Sector[][] sectors = new Sector[SIZENUMBER][SIZELETTER];
 
     /**
-     * The constructor.
+     * The Constructor
+     * 
+     * @param sectors sectors of the map
      */
     public Map(Sector[][] sectors) {
         this.sectors=sectors;
     }
+    
     /**
      * Returns max rows of the map.
      * 
@@ -92,68 +58,4 @@ public class Map {
     public Sector[][] getSector() {
         return this.sectors;
     }
-
-    /**
-     * Returns sectorDangerous.
-     * 
-     * @return sectorDangerous
-     */
-    public SectorDangerous getSectorDangerous() {
-        return this.sectorDangerous;
-    }
-
-    /**
-     * Returns sectorSecure.
-     * 
-     * @return sectorSecure
-     */
-    public SectorSecure getSectorSecure() {
-        return this.sectorSecure;
-    }
-
-    /**
-     * Returns sectorAlien.
-     * 
-     * @return sectorAlien
-     */
-    public SectorAlien getSectorAlien() {
-        return this.sectorAlien;
-    }
-
-    /**
-     * Returns sectorVoid.
-     * 
-     * @return sectorVoid
-     */
-    public SectorVoid getSectorVoid() {
-        return this.sectorVoid;
-    }
-
-    /**
-     * Sets a value to attribute sectorVoid.
-     * 
-     * @param newSectorVoid
-     */
-    public void setSectorVoid(SectorVoid newSectorVoid) {
-        this.sectorVoid = newSectorVoid;
-    }
-
-    /**
-     * Returns sectorHuman.
-     * 
-     * @return sectorHuman
-     */
-    public SectorHuman getSectorHuman() {
-        return this.sectorHuman;
-    }
-
-    /**
-     * Returns sectorEscapeHatch.
-     * 
-     * @return sectorEscapeHatch
-     */
-    public SectorEscapeHatch getSectorEscapeHatch() {
-        return this.sectorEscapeHatch;
-    }
-
 }
