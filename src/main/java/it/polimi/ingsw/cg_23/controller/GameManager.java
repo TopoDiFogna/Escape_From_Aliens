f35c5e34d124beab1fa65a230065ad9b566460a8
Paolo Paterna <paolo.paterna@gmail.com>
2015-05-22 19:46:02 +0200

    Starting implementing Server with sockets

--- src/main/java/it/polimi/ingsw/cg_23/controller/GameManager.java
+++ src/main/java/it/polimi/ingsw/cg_23/controller/GameManager.java
@@ -1,9 +1,25 @@
 package it.polimi.ingsw.cg_23.controller;
 
-public class GameManager {
+import java.net.Socket;
+import java.util.Observable;
+import java.util.Observer;
 
-    public GameManager() {
+public class GameManager extends Observable implements Observer, Runnable{
+
+    public GameManager(Socket socket) {
         // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        
     }
 
 }
