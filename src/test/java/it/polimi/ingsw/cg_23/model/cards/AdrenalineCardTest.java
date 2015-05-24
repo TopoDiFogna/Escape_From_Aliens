3e953d7e47979a6ce1f07de805b876d93c4ed866
Arianna Zappa <zappa.arianna@gmail.com>
2015-05-24 20:21:25 +0200

    Create DeckFactoryTest

---
+++ src/test/java/it/polimi/ingsw/cg_23/model/cards/AdrenalineCardTest.java
@@ -0,0 +1,35 @@
+package it.polimi.ingsw.cg_23.model.cards;
+
+import static org.junit.Assert.*;
+
+import java.net.Socket;
+
+import it.polimi.ingsw.cg_23.controller.GameLogic;
+import it.polimi.ingsw.cg_23.model.players.Human;
+import it.polimi.ingsw.cg_23.model.players.Player;
+import it.polimi.ingsw.cg_23.model.status.Match;
+
+import org.junit.Test;
+
+public class AdrenalineCardTest {
+
+	@Test
+	public void testDoAction() {
+		Player player = new Human("dummy");
+		Card card = new AdrenalineCard();
+		String mapName = "galilei";
+		Socket socket = new Socket();
+		Match match = new Match(mapName, player, socket);
+		GameLogic controller = new GameLogic(match);
+		card.doAction(player, controller);
+		assertTrue(player.getCanMoveFaster());
+		
+	}
+
+	@Test
+	public void testAdrenalineCard() {
+		Card card = new AdrenalineCard();
+		assertNotNull(card);
+	}
+
+}
