408cbc1f6ee6667a5c381e074852055869a514e8
Arianna Zappa <zappa.arianna@gmail.com>
2015-05-26 17:38:50 +0200

    Deleted useless App class and AppTest. Created lots of test in controller. Changed some methods in GameLogic.

--- src/test/java/it/polimi/ingsw/cg_23/controller/GameLogicTest.java
+++ src/test/java/it/polimi/ingsw/cg_23/controller/GameLogicTest.java
@@ -1,9 +1,20 @@
 package it.polimi.ingsw.cg_23.controller;
 
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import it.polimi.ingsw.cg_23.model.cards.AttackCard;
 import it.polimi.ingsw.cg_23.model.cards.Card;
+import it.polimi.ingsw.cg_23.model.cards.Deck;
+import it.polimi.ingsw.cg_23.model.cards.DeckFactory;
+import it.polimi.ingsw.cg_23.model.cards.NoiseInAnySectorCard;
+import it.polimi.ingsw.cg_23.model.cards.NoiseInYourSectorCard;
 import it.polimi.ingsw.cg_23.model.cards.SedativesCard;
 import it.polimi.ingsw.cg_23.model.cards.TeleportCard;
+import it.polimi.ingsw.cg_23.model.map.Sector;
+import it.polimi.ingsw.cg_23.model.map.SectorTypeEnum;
+import it.polimi.ingsw.cg_23.model.players.Alien;
 import it.polimi.ingsw.cg_23.model.players.Human;
 import it.polimi.ingsw.cg_23.model.players.Player;
 import it.polimi.ingsw.cg_23.model.status.Match;
@@ -69,14 +80,21 @@ public class GameLogicTest {
 	/*@Test
 	public void testUseOtherCard() {
 		fail("Not yet implemented");
-	}
+	}*/
 
 	@Test
 	public void testDiscardCard() {
-		fail("Not yet implemented");
+		Player player = new Human("dummy");
+		Card card = new SedativesCard();
+		Match match = new Match("galilei");
+		GameLogic controller = new GameLogic(match);
+		player.getCards().add(card);
+		controller.discardCard(player, card);
+		assertFalse(player.getCards().contains(card));
+		assertTrue(match.getItemDeckDiscarded().contains(card));
 	}
 
-	@Test
+	/*@Test
 	public void testDrawEscapeHatchCard() {
 		fail("Not yet implemented");
 	}
@@ -84,19 +102,51 @@ public class GameLogicTest {
 	@Test
 	public void testDrawSectorCard() {
 		fail("Not yet implemented");
-	}
+	}*/
 
 	@Test
 	public void testShuffleSectorDeck() {
-		fail("Not yet implemented");
+		int count = 0;
+		Card card1 = new NoiseInAnySectorCard(false);
+		Card card2 = new NoiseInAnySectorCard(false);
+		Card card3 = new NoiseInAnySectorCard(true);
+		Match match = new Match("galilei");
+		GameLogic controller = new GameLogic(match);
+		Deck<Card> sectorDeckDiscarded = DeckFactory.createDeck(3);
+		match.setSectorDeckDiscarded(sectorDeckDiscarded);
+		match.getSectorDeckDiscarded().add(card1);
+		match.getSectorDeckDiscarded().add(card2);
+		match.getSectorDeckDiscarded().add(card3);
+		controller.shuffleSectorDeck();
+		for (Card card : sectorDeckDiscarded) {
+			count ++;
+			assertTrue(card instanceof NoiseInAnySectorCard);
+		}
+		assertEquals(3, count);
 	}
 
 	@Test
 	public void testShuffleItemDeck() {
-		fail("Not yet implemented");
+		int count = 0;
+		Card card1 = new AttackCard();
+		Card card2 = new AttackCard();
+		Card card3 = new AttackCard();
+		Match match = new Match("galilei");
+		GameLogic controller = new GameLogic(match);
+		Deck<Card> itemDeckDiscarded = DeckFactory.createDeck(3);
+		match.setItemDeckDiscarded(itemDeckDiscarded);
+		match.getItemDeckDiscarded().add(card1);
+		match.getItemDeckDiscarded().add(card2);
+		match.getItemDeckDiscarded().add(card3);
+		controller.shuffleSectorDeck();
+		for (Card card : itemDeckDiscarded) {
+			count ++;
+			assertTrue(card instanceof AttackCard);
+		}
+		assertEquals(3, count);
 	}
 
-	@Test
+	/*@Test
 	public void testPickSectorCard() {
 		fail("Not yet implemented");
 	}
@@ -116,4 +166,57 @@ public class GameLogicTest {
 		fail("Not yet implemented");
 	}*/
 
+	@Test
+	public void testRemoveAfterDyingTrue() {
+		Player player = new Alien("dummy");
+		Match match = new Match("galilei");
+		Sector[][] map = match.getMap().getSector();
+		GameLogic controller = new GameLogic(match);
+		player.setCurrentSector(map[1][1]);
+		match.addNewPlayerToList(player);
+		player.setDead();
+		controller.removeAfterDying(player);
+		assertFalse(player.isAlive());
+		assertFalse(player.getCurrentSector().getPlayer().contains(player));
+		assertFalse(match.getPlayers().contains(player));
+	}
+	
+	@Test
+	public void testRemoveAfterDyingFalse() {
+		Player player = new Alien("dummy");
+		Match match = new Match("galilei");
+		Sector[][] map = match.getMap().getSector();
+		GameLogic controller = new GameLogic(match);
+		player.setCurrentSector(map[1][1]);
+		match.addNewPlayerToList(player);
+		controller.removeAfterDying(player);
+		assertTrue(player.isAlive());
+	}
+	
+	@Test
+	public void testRemoveAfterWinningTrue(){
+		Player player = new Alien("dummy");
+		Match match = new Match("galilei");
+		Sector sector = new Sector(2, 13, SectorTypeEnum.ESCAPEHATCH, true);
+		GameLogic controller = new GameLogic(match);
+		player.setCurrentSector(sector);
+		match.addNewPlayerToList(player);
+		player.getCurrentSector().setEscapeHatchSectorNotCrossable();
+		controller.removeAfterWinning(player);
+		assertFalse(sector.isCrossable());
+		assertFalse(player.getCurrentSector().getPlayer().contains(player));
+		assertFalse(match.getPlayers().contains(player));
+	}
+	
+	@Test
+	public void testRemoveAfterWinningFalse(){
+		Player player = new Alien("dummy");
+		Match match = new Match("galilei");
+		Sector sector = new Sector(2, 13, SectorTypeEnum.ESCAPEHATCH, true);
+		GameLogic controller = new GameLogic(match);
+		player.setCurrentSector(sector);
+		match.addNewPlayerToList(player);
+		controller.removeAfterWinning(player);
+		assertTrue(sector.isCrossable());
+	}
 }
