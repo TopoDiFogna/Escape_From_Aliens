9ad6092854147bdb3f1443d869e10616b86b1efa
Paolo Paterna <paolo.paterna@gmail.com>
2015-06-13 13:28:01 +0200

    added javadoc to serverstatus class

--- src/main/java/it/polimi/ingsw/cg_23/network/ServerStatus.java
+++ src/main/java/it/polimi/ingsw/cg_23/network/ServerStatus.java
@@ -5,25 +5,49 @@ import it.polimi.ingsw.cg_23.network.rmi.RMIBroker;
 import it.polimi.ingsw.cg_23.network.socket.SocketBroker;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class ServerStatus {
     
+    /**
+     * The instance of this class since this is a singleton
+     */
     private static ServerStatus instance;
     
-    private HashMap<String, Match> idLogicMap;
+    /**
+     * Map for mapping unic client id and hia match
+     */
+    private Map<String, Match> idLogicMap;
     
-    private HashMap<Match, SocketBroker> logicSocketBrokerMap;
+    /**
+     * Mapping for every match with its socket broker
+     */
+    private Map<Match, SocketBroker> logicSocketBrokerMap;
     
+    /**
+     * Mapping for every match with its rmi broker
+     */
+    private Map<Match, RMIBroker> logicRMIBrokerMap;
+    
+    /**
+     * Number used to make broker topics unique
+     */
     private int brokerNumber = 0;
 
-    private HashMap<Match, RMIBroker> logicRMIBrokerMap;
-
+    /**
+     * Initializes every map used
+     */
     private ServerStatus() {
         idLogicMap= new HashMap<>();
         logicSocketBrokerMap= new HashMap<>();
         logicRMIBrokerMap= new HashMap<>();
     }
 
+    /**
+     * This method returns the only instance of this class or creates it if it doesn't exists
+     * 
+     * @return the only instance of this class
+     */
     public static ServerStatus getInstance(){
         if(instance==null){
             synchronized (ServerStatus.class) {
@@ -35,34 +59,70 @@ public class ServerStatus {
         return instance;
     }
     
+    /**
+     * Adds the unique id of a player to a match
+     * 
+     * @param name the unique id of the player
+     * @param match match the player is added to
+     */
     public void addPlayerToMatch(String name, Match match){
         idLogicMap.put(name, match);
     }
     
+    /**
+     * Pairs every match with his socket broker
+     * 
+     * @param match match to be paired
+     * @param broker broker associated to the match
+     */
     public void addSocketBrokerToMatch(Match match, SocketBroker broker){
         logicSocketBrokerMap.put(match, broker);
     }
 
-    public HashMap<String, Match> getIdMatchMap() {
+    /**
+     * Returns the entire map player id match
+     * 
+     * @return the map linking every player to his match
+     */
+    public Map<String, Match> getIdMatchMap() {
         return idLogicMap;
     }
 
-    public HashMap<Match, SocketBroker> getMatchSocketBrokerMap() {
+    /**
+     * Returns the entire map match, socket broker
+     * 
+     * @return the map linking every match to its socket broker
+     */
+    public Map<Match, SocketBroker> getMatchSocketBrokerMap() {
         return logicSocketBrokerMap;
     }
     
-    public HashMap<Match, RMIBroker> getMatchRMIBrokerMap() {
+    /**
+     * Returns the entire map match, rmi broker
+     * 
+     * @return the map linking every match to its rmi broker
+     */
+    public Map<Match, RMIBroker> getMatchRMIBrokerMap() {
         return logicRMIBrokerMap;
     }
     
+    /**
+     * Pairs every match with his rmi broker
+     * 
+     * @param match match to be paired
+     * @param broker broker associated to the match
+     */
    public void addRMIBrokerToMatch(Match match, RMIBroker broker){
         logicRMIBrokerMap.put(match, broker);
     }
 
+   /**
+    * This method makes sure that every broker has a unique id first incrementing the number used for id then returning it
+    * 
+    * @return the unique id of the next broker
+    */
     public int getBrokerNumber() {
         brokerNumber++;
         return brokerNumber;
     }
-    
-    
 }
