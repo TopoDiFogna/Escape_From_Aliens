2252ce6226337365ae986fdeabe0d1570086fea3
Arianna Zappa <zappa.arianna@gmail.com>
2015-06-19 17:03:07 +0200

    Added missing import

--- src/main/java/it/polimi/ingsw/cg_23/gui/ChatPanel.java
+++ src/main/java/it/polimi/ingsw/cg_23/gui/ChatPanel.java
@@ -14,6 +14,7 @@ import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.ScrollPaneConstants;
 
